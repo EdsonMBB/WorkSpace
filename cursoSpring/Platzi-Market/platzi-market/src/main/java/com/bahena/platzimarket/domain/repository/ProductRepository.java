15a89c0490b1392d87f48ee354b7b812c9a32ee4
Edson Miguel Bahena Barrios <edson.bahena.barrios@accenture.com>
2021-07-26 12:30:18 -0500

    Curso Spring 1

---
+++ cursoSpring/Platzi-Market/platzi-market/src/main/java/com/bahena/platzimarket/domain/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.bahena.platzimarket.domain.repository;
+
+
+
+import com.bahena.platzimarket.domain.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> getAll();
+    Optional<List<Product>> getByCategory(int categoryId);
+    Optional<List<Product>> getScarceProducts(int quantity);
+    Optional<Product> getProduct(int productId);
+    Product save(Product product);
+    void delete(int productId);
+}
